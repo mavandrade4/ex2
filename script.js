66880a8029127410c39a41b8919e7c373f540a7e
mavandrade4 <mavandrade4@gmail.com>
2023-12-26 17:11:49 +0000

    text search working - tirei data e rating filters

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const JSON_PATH = 'books.json';
-
-//////////////////
+const JSON_PATH = 'books1.json';
 
 //////// SORT FUNC
 
@@ -9,12 +7,6 @@ const SORT_YEAR_DESC = function(a, b) {
 };
 const SORT_RATING_DESC = function(a, b) {
   return parseFloat(b.average_rating) - parseFloat(a.average_rating);
-};
-
-const SORT_ALPHA_PUBLISHER = function(a, b) {
-  const publisherA = (a.publisher || '').toUpperCase();
-  const publisherB = (b.publisher || '').toUpperCase();
-  return publisherA.localeCompare(publisherB);
 };
 const SORT_ALPHA_AUTHOR = function(a, b) {
   const titleA = a.authors.toUpperCase();
@@ -31,35 +23,48 @@ const SORT_ALPHA_TITLE = function(a, b) {
   return 0;
 };
 
-//////////////////
+////////////////// APP
 
 class App {
   constructor() {
     this._onJsonReady = this._onJsonReady.bind(this);
     this._sortBooks = this._sortBooks.bind(this);
-    
+
+    // SORT
+
     this.headerClickHandlers = {
       'alpha': SORT_ALPHA_TITLE,
       'author': SORT_ALPHA_AUTHOR,
       'year': SORT_YEAR_DESC,
       'rating': SORT_RATING_DESC,
-      'publisher': SORT_ALPHA_PUBLISHER,
     };
 
+    // SEARCH INPUT
+
+    this.minRatingInput = document.querySelector('#minRating');
+    this.minRatingInput.addEventListener('input', () => this._renderBooks());
+    this.maxRatingInput = document.querySelector('#maxRating');
+    this.maxRatingInput.addEventListener('input', () => this._renderBooks());
+    
+    // RESIZE
+
+    this.handleWindowResize = this.handleWindowResize.bind(this);
+    window.addEventListener('resize', this.handleWindowResize);
+    
     this.initializeHeaderClickHandlers();
   }
 
+  // RESIZE
   handleWindowResize() {
     this.adjustTableSize();
   }
-
   adjustTableSize() {
     const table = document.querySelector('#myTable');
     const windowWidth = window.innerWidth;
-
     table.style.width = (windowWidth - 20) + 'px';
   }
   
+  // SORT
   initializeHeaderClickHandlers() {
     const headers = document.querySelectorAll('#myTable th');
     headers.forEach(header => {
@@ -67,87 +72,45 @@ class App {
       header.addEventListener('click', () => this.handleHeaderClick(headerId));
     });
 
-    ////// SEARCH
+    /// SEARCH
     // TEXT
     const searchInput = document.querySelector('#searchInput');
     searchInput.addEventListener('input', () => this.handleSearchInput());
     // DATE
-    const dateFilter = document.querySelector('#dateFilter');
-    dateFilter.addEventListener('input', () => this._renderBooks());
+    this.dateFilter = document.querySelector('#dateFilter');
+    this.dateFilter.addEventListener('input', () => this._renderBooks());
     // RATING
     const minRatingInput = document.querySelector('#minRating');
     minRatingInput.addEventListener('input', () => this._renderBooks());
-
     const maxRatingInput = document.querySelector('#maxRating');
     maxRatingInput.addEventListener('input', () => this._renderBooks());
   }
 
+  // SORT
   handleHeaderClick(headerId) {
     const sortFunction = this.headerClickHandlers[headerId];
     if (sortFunction) {
       this._sortBooks(sortFunction);
     }
   }
-
-  handleSearchInput() {
-    this._renderBooks();
-  }
-  
   _sortBooks(sortFunction) {
     this.bookInfo.sort(sortFunction);
     this._renderBooks();
   }
 
-  _renderBooks() {
-    const table = document.querySelector('#myTable');
-    const searchInput = document.querySelector('#searchInput');
-    //TEXT
-    const searchTerm = searchInput.value.trim().toUpperCase();
-    //DATE
-    const selectedDate = dateFilter.value;
-    /*/RATING
-    const minRating = parseFloat(minRatingInput.value) || 0;
-    const maxRating = parseFloat(maxRatingInput.value) || 6;
-    */
-
-    // Clear existing rows
-    table.querySelectorAll('tr:not(.header)').forEach(row => row.remove());
-
-    for (const info of this.bookInfo) {
-      if (
-        info.title.toUpperCase().includes(searchTerm) ||
-        info.authors.toUpperCase().includes(searchTerm) ||
-        info.publisher.toUpperCase().includes(searchTerm) ||
-         (!selectedDate || new Date(info.publication_date) >= new Date(selectedDate)) || 
-         (info.average_rating >= minRating && info.average_rating <= maxRating)
-      ) {
-        const row = table.insertRow(-1);
-
-        const cell1 = row.insertCell(0);
-        cell1.innerHTML = info.title;
-
-        const cell2 = row.insertCell(1);
-        cell2.innerHTML = info.authors;
-
-        const cell3 = row.insertCell(2);
-        cell3.innerHTML = info.publication_date;
-
-        const cell4 = row.insertCell(3);
-        cell4.innerHTML = info.average_rating;
-
-        const cell5 = row.insertCell(4);
-        cell5.innerHTML = info.publisher;
-      }
-    }
+  // SEARCH
+  handleSearchInput() {
+    this._renderBooks();
   }
 
+  // LOAD
   loadBooks() {
     fetch(JSON_PATH)
         .then(this._onResponse)
         .then(this._onJsonReady);
   }
-
   _onJsonReady(json) {
+    console.log('Books loaded:', json);
     if (Array.isArray(json.books)) {
       this.bookInfo = json.books;
       this._renderBooks();
@@ -155,9 +118,46 @@ class App {
       console.error('Invalid book data:', json.books);
     }
   }
-
   _onResponse(response) {
     return response.json();
+  }
+
+  // RENDER BOOKS
+  _renderBooks() {
+    const table = document.querySelector('#myTable');
+    const searchInput = document.querySelector('#searchInput');
+    // TEXT
+    const searchTerm = searchInput.value.trim().toUpperCase();
+    // DATE
+    const selectedDate = this.dateFilter.value;
+    // RATING
+    const minRating = parseFloat(this.minRatingInput.value) || 0;
+    const maxRating = parseFloat(this.maxRatingInput.value) || 6;
+  
+    // Clear existing rows
+    table.querySelectorAll('tr:not(.header)').forEach(row => row.remove());
+  
+    let count = 0;
+  
+    this.bookInfo.forEach(info => {
+      const titleMatches = info.title.toUpperCase().includes(searchTerm);
+      const authorMatches = info.authors.toUpperCase().includes(searchTerm);
+      //const dateMatches = !selectedDate || new Date(info.publication_date) >= new Date(selectedDate);
+      //const ratingMatches = info.average_rating >= minRating && info.average_rating <= maxRating;
+  
+      //|| dateMatches || ratingMatches
+      if (titleMatches || authorMatches) { 
+        const row = table.insertRow(-1);
+        const cell1 = row.insertCell(0);
+        cell1.innerHTML = info.title;
+        const cell2 = row.insertCell(1);
+        cell2.innerHTML = info.authors;
+        const cell3 = row.insertCell(2);
+        cell3.innerHTML = info.publication_date;
+        const cell4 = row.insertCell(3);
+        cell4.innerHTML = info.average_rating;
+      }
+    });
   }
 }
 
